81b70730f7ef1383cf8d771f3f20b6e27331b14a
syjs10 <350172898@qq.com>
2018-01-22 16:17:01 +0800

    微信关注时回复消息

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -28,49 +28,36 @@ class Index
     }
     public function responseMsg()
     {
-        //1.获取到微信推送过来post数据（xml格式）
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
-        //2.处理消息类型，并设置回复类型和内容
-        /*<xml>
-        <ToUserName><![CDATA[toUser]]></ToUserName>
-        <FromUserName><![CDATA[FromUser]]></FromUserName>
-        <CreateTime>123456789</CreateTime>
-        <MsgType><![CDATA[event]]></MsgType>
-        <Event><![CDATA[subscribe]]></Event>
-        </xml>*/
+        // 获取Xml格式消息
+        $postArr = file_get_contents('php://input');
+        //Xml转化成对象
         $postObj = simplexml_load_string($postArr);
-        //$postObj->ToUserName = '';
-        //$postObj->FromUserName = '';
-        //$postObj->CreateTime = '';
-        //$postObj->MsgType = '';
-        //$postObj->Event = '';
-        // gh_e79a177814ed
-        //判断该数据包是否是订阅的事件推送
+
+        // <xml>
+        //     <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+        //     <FromUserName>< ![CDATA[fromUser] ]></FromUserName>
+        //     <CreateTime>1348831860</CreateTime>
+        //     <MsgType>< ![CDATA[text] ]></MsgType>
+        //     <Content>< ![CDATA[this is a test] ]></Content>
+        //     <MsgId>1234567890123456</MsgId>
+        // </xml>
         if (strtolower($postObj->MsgType) == 'event') {
-            //如果是关注 subscribe 事件
-            if (strtolower($postObj->Event == 'subscribe')) {
-                //回复用户消息(纯文本格式)
+            if (strtolower($postObj->Event) == 'subscribe') {
                 $toUser   = $postObj->FromUserName;
-                $fromUser = $postObj->ToUserName;
+                $fromUser = $postObj->toUserName;
                 $time     = time();
                 $msgType  = 'text';
-                $content  = '欢迎关注我们的微信公众账号' . $postObj->FromUserName . '-' . $postObj->ToUserName;
+                $content  = '关注测试';
                 $template = "<xml>
-                            <ToUserName><![CDATA[%s]]></ToUserName>
-                            <FromUserName><![CDATA[%s]]></FromUserName>
-                            <CreateTime>%s</CreateTime>
-                            <MsgType><![CDATA[%s]]></MsgType>
-                            <Content><![CDATA[%s]]></Content>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
                             </xml>";
                 $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
                 echo $info;
-/*<xml>
-<ToUserName><![CDATA[toUser]]></ToUserName>
-<FromUserName><![CDATA[fromUser]]></FromUserName>
-<CreateTime>12345678</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[你好]]></Content>
-</xml>*/
+
             }
         }
     }
