28e6a51fe5b6b62cbcf167c8b33df41dc104f13d
syjs10 <350172898@qq.com>
2018-01-22 15:56:38 +0800

    微信关注时回复消息

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -12,15 +12,53 @@ class Index
         $timestamp = $_GET['timestamp'];
         $nonce     = $_GET['nonce'];
         $tokenn    = "syjs10test";
+        $echostr   = $_GET['echostr'];
         $signature = $_GET['signature'];
         $array     = array($timestamp, $nonce, $tokenn);
         sort($array);
         $tmpstr = implode('', $array);
         $tmpstr = sha1($tmpstr);
-        if ($tmpstr == $signature) {
+        if ($tmpstr == $signature && $echostr) {
+            //第一次接入微信API
             echo $_GET['echostr'];
-            exit();
+            exit;
+        } else {
+            $this->responseMsg();
         }
+    }
+    public function responseMsg()
+    {
+        // 获取Xml格式消息
+        $postArr = $_GLOBALS['PHP_RAW_POST_DATA'];
+        //Xml转化成对象
+        $postObj = simplexml_load_string($postArr);
 
+        // <xml>
+        //     <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+        //     <FromUserName>< ![CDATA[fromUser] ]></FromUserName>
+        //     <CreateTime>1348831860</CreateTime>
+        //     <MsgType>< ![CDATA[text] ]></MsgType>
+        //     <Content>< ![CDATA[this is a test] ]></Content>
+        //     <MsgId>1234567890123456</MsgId>
+        // </xml>
+        if (strtolower($postObj->MsgType) == 'event') {
+            if ($postObj->Event == 'subscribe') {
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  = 'text';
+                $content  = '关注测试';
+                $template = "<xml>
+                                <ToUserName>< ![CDATA[%s] ]></ToUserName>
+                                <FromUserName>< ![CDATA[%s] ]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType>< ![CDATA[%s] ]></MsgType>
+                                <Content>< ![CDATA[%s] ]></Content>
+                            </xml>";
+                $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+
+            }
+        }
     }
 }
