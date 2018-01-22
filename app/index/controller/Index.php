ebb2ba561c314ab88b9217fbc54cdacd847e27eb
syjs10 <350172898@qq.com>
2018-01-22 14:43:02 +0800

    testing

---
+++ app/index/controller/Index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ */
+class Index
+{
+
+    public function index()
+    {
+        $timestamp = $_GET['timestamp'];
+        $nonce     = $_GET['nonce'];
+        $tokenn    = "syjs10test";
+        $signature = $_GET['signature'];
+        $array     = array($timestamp, $nonce, $tokenn);
+        sort($array);
+        $tmpstr = implode('', $array);
+        $tmpstr = sha1($tmpstr);
+        if ($tmpstr == $signature) {
+            echo $_GET['echostr'];
+            exit();
+        }
+    }
+}
---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    // 应用调试模式
+    'app_debug'        => true,
+    'auto_bind_moudle' => true,
+    'url_route_on'     => true,
+];
