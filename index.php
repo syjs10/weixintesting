2a13d1a5ee092ce91ac1040b49f4f8afd4ef8e8a
syjs10 <350172898@qq.com>
2018-01-22 14:24:11 +0800

    testing

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo 'testing' ?>
+<?php
+
+$timestamp = $_GET['timestamp'];
+$nonce     = $_GET['nonce'];
+$tokenn    = "syjs10test";
+$signature = $_GET['signature'];
+$array     = array($timestamp, $nonce, $tokenn);
+sort($array);
+$tmpstr = implode('', $array);
+$tmpstr = sha1($tmpstr);
+if ($tmpstr == $signature) {
+    echo $_GET['echostr'];
+    exit();
+}
