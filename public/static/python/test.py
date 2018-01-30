b1e2445f02ffa777cdc7b72609ec6d4b13778b81
syjs10 <350172898@qq.com>
2018-01-30 18:58:13 +0800

    常用方法归入Model

--- public/static/python/test.py
+++ public/static/python/test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#
+
 import requests
-import pytesseract
-from PIL import Image
-from bs4 import BeautifulSoup
-import json
+# import pytesseract
+# from PIL import Image
+# from bs4 import BeautifulSoup
+# import json
 # import sys
 
 # reload(sys)
